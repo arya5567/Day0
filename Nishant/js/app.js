68d19c4a5a34160970fc33ff871181c9b1278b82
Nishant chaudhary <nishantjaat196@gmail.com>
2024-06-11 12:06:46 +0530

    nishant code

---
+++ Nishant/js/app.js
@@ -0,0 +1,292 @@
+/*var a= "hello world";
+/*var a= "hello world";
+
+function b() { 
+var d= " NISHANT CHAUDHARY "
+// debugger;
+}
+console.log(x);
+
+var x = function() {
+	console.log("I am X");
+}2
+console.log(x);
+
+x();
+
+a();
+
+
+
+
+ function b(){
+	 console.log(myvar);
+ }
+ function a(){
+	 var myvar =2;
+	 console.log(myvar);
+	 b();
+ }
+ var myvar=1;
+ c0onsole.log(myvar) ;
+ +
+ a();
+ co0nsole.log(myvar)'  ;
+ 
+ 
+   let i;
+   for(i=1;i<=100;i++) {
+	  
+ if(i%2!=0)
+  i
+console.log(i); 
+  }
+  */
+  /*
+ function isPrime(n) {
+    if (n <= 1) 
+    if (n == 2 || n == 3)   
+    if (n % 2 == 0 || n % 3 == 0) 
+ 
+    for (var i=5;i <=(n);i=i+6) 
+        if (n%i==0||n%(i++)==0)
+   console.log (prime(n)) ;			
+ }
+ */
+ /*
+ if (true==!undefined){
+	 console.log("true");
+ }
+ else
+ {
+	 console.log("false");
+ } 
+ 
+ 
+ var obj ={"name":"NISHNAT CHAUDHARY"};
+  console.log(obj); 
+ var abc = obj;
+ console.log(abc); 
+ obj.name= "mayank";
+ console.log(obj); 
+ console.log(abc);
+    
+
+
+ let i,j;
+ 
+   for(i=2;i<=100;i++)
+	   
+   {
+	   var c=0;
+	   
+	   for(j=2;j>=i;j++)
+	   {
+		   if(i%j==0)
+		   {
+			   c++;
+		   }	  
+	   }
+	   if(c==1)
+		   console.log(i);
+   }
+
+	   let count=0;
+	   let i,j;
+	   for(j=2;j<=100;j++)
+	   {
+		   for(i=1;i<=j;j++){
+			   if(j%i==0)
+				   count++;
+		   }
+		   if(count==2)
+			   console.log(j);
+		   count=0;
+	   }
+	   function greet(firstname='nishant',lastname='choudhary'){
+		   console.log(firstname);
+		   console.log(lastname);
+	   }
+	   greet();  
+	
+      
+ 
+	   
+	   function greet(firstname){
+		   console.log(firstname);
+		   
+	   }
+	   function greet(){
+    		   console.log('hello');
+    		   
+		   } 
+		   function greet(firstname,lastname){
+		   console.log(firstname);
+		   console.log(lastname);
+		   }
+	   
+	    
+	   	   
+		   greet('nishant','choudhary');
+	   greet('nishant');
+	   greet();
+
+ function abc(){
+	 return {
+		 name:"arya"
+	 }
+ }
+ 
+ 
+ function abc1 (){
+ 	 return 
+	 {
+ 		 name:"arya"
+ 	 }
+  }
+ console.log(abc());
+ console.log(abc1());
+ 
+
+ 
+
+
+
+let i=5 ;
+function fact(n)  {
+	
+fact(n) =1;
+   if( n==0)
+for (let i=2; i<=n;i++)
+	fact(n)= fact(n)*i;
+}
+
+console.log(i); 
+function factorial(n) { 
+
+    let fact = 1; 
+
+     
+
+    if(n === 0)
+
+        return 1;
+
+    for (let i = 2; i <= n; i++) 
+
+        fact = fact* i; 
+
+    return fact;
+	
+}
+console.log(factorial(n));
+ let n = 10; 
+ 
+ for(var i=1;i<=10;i++){
+	 setTimeout((i)=>{
+	 console.log(i);
+	 },1000);
+ }*/
+ 
+ 
+// var i=1;
+ // for(;i<=10;){
+//	 console.log(i);
+//i++;	 
+// }
+// console.log(i);
+
+ //setTimeout(function()=>{},1000);
+
+/*
+
+
+
+
+let a =0;
+let b=1;
+let c;
+let n=100;
+for(i=0;i<=n;i++){
+	console.log(a);
+	c=a+b;
+	a=b;
+	b=c;
+}
+
+
+
+
+
+let a=2;	
+let num=prompt("enter the number you want to check if it is  prime");
+
+let count=0;
+if(num<2){
+	console.log("not a prime number");
+}
+for(i=2;i<=num;i++){
+	if(num%i==0){
+		count++;
+	}
+}
+if(count>=2){
+	console.log("number is not prime");
+}
+else
+	console.log("number is prime");
+
+let a=0;
+let b=1;
+let n=10;
+for(i=0;i<n;i++){
+    a++;
+    b=a*b;
+}
+console.log(b);
+
+
+
+ let a=40;
+ let b=10;
+ let c;
+    // assign a value 
+	   c=a;
+	   a=b;
+	  b=c;
+	  console.log("a=",a);
+	  console.log("b=",b);
+	  
+	  
+	  
+  var string = prompt;
+ console.log (string);
+ 
+	
+	 
+	  
+	  
+*/
+ function heelo(){
+	 console.log("hello world");
+	 
+ }
+	 heelo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	  
+	  
+	  
+	  
